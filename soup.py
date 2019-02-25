9ddf2244ee4393761f4c1a3796354094dff47e0a
Devansh Soni <42613455+DevanshSoni@users.noreply.github.com>
2019-02-25 23:08:51 +0530

    Update soup.py

--- soup.py
+++ soup.py
@@ -8,31 +8,11 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen
 html=urlopen("https://www.sololearn.com")
 soup=BeautifulSoup(html,"lxml")
-title=soup.title
+title=soup.title    #getting title of the webpage
 print(title)
 
 
 # In[17]:
 
-
-a=soup.find_all("a")
-for i in a:
-    print(i.get("href"))
-
-
-# In[19]:
-
-
-# for getting all "a" tags
-a=soup.find_all
-soup.find_all("a")
-
-
-# In[39]:
-
-
-html=urlopen("https://www.tutorialspoint.com/html/html_tables.htm")
-soup=BeautifulSoup(html,"lxml")
-head=soup.find_all("li")
-print(head[10])
-
+for i in soup.find_all("a", href=True):   
+    print(i["href"])      #getting all the links from the webpage
