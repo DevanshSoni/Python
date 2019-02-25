3dbb207efedf728dcabd877d0ef19e47eff28825
Devansh Soni <42613455+DevanshSoni@users.noreply.github.com>
2019-02-25 23:16:31 +0530

    Update soup.py

--- soup.py
+++ soup.py
@@ -1,18 +1,15 @@
 
 # coding: utf-8
-
 # In[13]:
-
-
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 html=urlopen("https://www.sololearn.com")
 soup=BeautifulSoup(html,"lxml")
 title=soup.title    #getting title of the webpage
 print(title)
-
-
 # In[17]:
-
-for i in soup.find_all("a", href=True):   
-    print(i["href"])      #getting all the links from the webpage
+for i in soup.find_all("a", href=True):  
+    if(i['href'][0]=='\'):
+        continue
+    else:  
+        print(i["href"])      #getting all the links from the webpage
